fa905ec4c252a28fa88fa62e67e08f2e8c613273
ArthurTuggle <arthurtuggle57@yahoo.com>
2020-12-08 20:12:30 +0000

    errors

--- person.cs
+++ person.cs
@@ -3,13 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 
-public class Person{
+public class Person
+{
    public string fname;
    public string lname;
    public string email;
    public Person(string fname,string lname,string email){
        this.fname=fname;
        this.lname=lname;
-       this.email=email;
    }
 }
